5bf6b82629692adf4ed5323472a3ed42e284057a
dwarowski <realcarenthusiast@yandex.ru>
2024-09-16 23:15:15 +0300

    new obj and script

--- Assets/Scripts/RoadMoving.cs
+++ Assets/Scripts/RoadMoving.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gor : MonoBehaviour
+public class RoadMoving : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
